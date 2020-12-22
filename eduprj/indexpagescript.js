c2dfca279e8367087ced6192ca4b87096aff1264
kesavaraman-s <68694485+kesavaraman-s@users.noreply.github.com>
2020-12-22 12:22:59 +0530

    Add files via upload

---
+++ eduprj/indexpagescript.js
@@ -0,0 +1,10 @@
+var containerElement=document.querySelector(".container");
+var topnav=document.querySelector(".topnav");
+var ilusimg=document.querySelector(".ilastrationimg");
+var loginform=document.querySelector(".loginform");
+
+function openLoginForm(){    
+    topnav.style.opacity="0.1";
+    ilusimg.style.opacity="0.1";
+    loginform.style.display="block";
+}
